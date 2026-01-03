7a922d7d4e3443ef3c5fa769c37a84e5343908b2
Sitharaj <sitharaj.info@gmail.com>
2026-01-03 08:16:50 +0530

    fix(ios): Disable strict Plist sanity check to resolve crash on high refresh rate devices

--- sample/src/iosMain/kotlin/com/sitharaj/reduxkmp/sample/MainViewController.kt
+++ sample/src/iosMain/kotlin/com/sitharaj/reduxkmp/sample/MainViewController.kt
@@ -2,4 +2,4 @@ package com.sitharaj.reduxkmp.sample
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(configure = { enforceStrictPlistSanityCheck = false }) { App() }
